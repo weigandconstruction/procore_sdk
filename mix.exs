4b67f0b9ed4d1a49851d5a1211e56ab9b8d0c638
Nate Todd <ntodd@weigandconstruction.com>
2024-06-10 11:30:28 -0400

    Bump to v0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Procore.Mixfile do
   def project do
     [
       app: :procore_sdk,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.15",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
